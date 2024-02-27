897a83ba4f99bb8a2560b64343229ca93f9b0213
이현제 <142381040+LEEHUYNJE@users.noreply.github.com>
2024-02-27 20:41:21 +0900

    Delete src directory

--- src/main/java/hello/core/member/Grade.java
+++
@@ -1,6 +0,0 @@
-package hello.core.member;
-
-public enum Grade { // 클래스 처럼 보이게하는 상수
-    BASIC,
-    VIP
-}
--- src/main/java/hello/core/member/MemberService.java
+++
@@ -1,7 +0,0 @@
-package hello.core.member;
-
-public interface MemberService {
-    void join(Member member); // member를 입력하면 id를 받고 회원이 되는 과정
-
-    Member findMember(Long memberId); // id를 통해서 맴버를 반환하는 함수
-}
